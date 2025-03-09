44bded2d4a52d9444c5df5bf5649e2199d66332b
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-03-09 21:07:16 +0200

    Used useParam to get ID for each van and  React.useeffect to used data fetched

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -12,13 +12,14 @@ import { Link } from "react-router-dom"
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}></Link>
+            <Link to={`/vans/${van.id}`}>
             <img src={van.imageUrl} />
             <div className="van-info">
                 <h3>{van.name}</h3>
                 <p>${van.price}<span>/day</span></p>
             </div>
             <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
     ))
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "/pages/Home"
-import About from "/pages/About"
-import Vans from "/pages/Vans"
-import "/server"
-import VanDetail from "/pages/VanDetail"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "/pages/Home";
+import About from "/pages/About";
+import Vans from "/pages/Vans";
+import "/server";
+import VanDetail from "/pages/VanDetail";
 
 function App() {
-
   return (
     <BrowserRouter>
       <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
         <nav>
           <Link to="/about">About</Link>
           <Link to="/vans">Vans</Link>
@@ -23,7 +24,7 @@ function App() {
         <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
